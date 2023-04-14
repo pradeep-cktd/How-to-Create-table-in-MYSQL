76ea486dcd3e785845142424945cf8c285f8cace
Pradeep Kumar (MERNSTACK DEVELOPER) <109032778+pradeep-cktd@users.noreply.github.com>
2023-04-14 20:09:19 +0530

    Add files via upload

---
+++ How to create tabe and Insert data into table.sql
@@ -0,0 +1,45 @@
+create table Employee (
+empid int primary key,
+name varchar(100),
+email varchar(100),
+salary int
+);
+insert into emp ( empid,name,email,salary) 
+values (
+ 1,
+'pradeep',
+'pradeep@herovired.com',
+50000
+);
+select * from emp;
+insert into emp values (
+5,
+'Sanjoy',
+'sanjoy@herovired.com',
+100000
+)
+insert into emp values (
+6,
+'Anjani',
+'anjani85@gmail.com',
+75000
+)
+insert into emp values (
+6,
+'Kushalpal',
+'Kushal@hotmail.com',
+45000
+)
+insert into emp values (
+7,
+'Sanjay_singh',
+'ssp@rediffmail.com',
+89000
+)
+insert into emp values (
+8,
+'Pradeep Kumar duwedi',
+'dgp@hotmail.com',
+65000
+)
+select * from emp;
